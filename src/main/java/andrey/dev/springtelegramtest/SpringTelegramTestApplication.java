61b7fcba9ac35f6ec90b30dc2cfcf5cc7b06098f
andrey <maltsevandreywork@gmail.com>
2025-11-17 21:44:27 +0300

    start

---
+++ src/main/java/andrey/dev/springtelegramtest/SpringTelegramTestApplication.java
@@ -0,0 +1,13 @@
+package andrey.dev.springtelegramtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTelegramTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTelegramTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/andrey/dev/springtelegramtest/SpringTelegramTestApplicationTests.java
@@ -0,0 +1,13 @@
+package andrey.dev.springtelegramtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTelegramTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
